143212e1c3a1269e8ef995cf144edebaf6c40705
Gulser Ozturk <gozturk@f1r5s17.codam.nl>
2023-09-06 14:48:15 +0200

    header added

--- include/minishell.h
+++ include/minishell.h
@@ -6,6 +6,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <unistd.h>
